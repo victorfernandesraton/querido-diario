78439ff906137d8a5e0b23ad60c2ce95f7e9353e
Victor Raton <43411882+victorfernandesraton@users.noreply.github.com>
2024-09-29 11:14:02 -0300

    wip: reimplementando raspador com base em novo site e requests

--- data_collection/gazette/spiders/al/al_maragogi.py
+++ data_collection/gazette/spiders/al/al_maragogi.py
@@ -1,8 +1,8 @@
-import re
+import locale
 from datetime import date, datetime
+from urllib.parse import urlencode
 
 import scrapy
-from dateutil.rrule import YEARLY, rrule
 
 from gazette.items import Gazette
 from gazette.spiders.base import BaseGazetteSpider
@@ -11,62 +11,68 @@ from gazette.spiders.base import BaseGazetteSpider
 class AlMaragogiSpider(BaseGazetteSpider):
     name = "al_maragogi"
     TERRITORY_ID = "2704500"
-    allowed_domains = ["maragogi.al.gov.br"]
-    base_url = "https://maragogi.al.gov.br/diarios-oficiais/diario-oficial-"
-    start_urls = ["https://maragogi.al.gov.br/diarios-oficiais/"]
-    start_date = date(2020, 1, 1)
-    end_date = date.today()
-    stop_crawling = False
-
-    def extrair_numero(self, arquivo):
-        match = re.search(r"no-(\d+)-", arquivo)
-        if match:
-            return match.group(1)
-        return None
+    allowed_domains = ["diario.maragogi.al.gov.br"]
+    start_date = date(2024, 4, 17)
+    start_urls = ["https://diario.maragogi.al.gov.br"]
 
     def start_requests(self):
-        for date_of_interest in rrule(
-            freq=YEARLY, dtstart=self.start_date, until=self.end_date
-        ):
-            base_url = f"{self.base_url}{date_of_interest.year}/"
-            yield scrapy.Request(url=base_url, callback=self.parse)
+        params = {
+            "BuscaSearch[data_inicio]": self.start_date.strftime("%Y-%m-%d"),
+            "BuscaSearch[data_fim]": self.end_date.strftime("%Y-%m-%d"),
+            "BuscaSearch[sort]": "data_new",
+            "BuscaSearch[modulo]": "diario-oficial",
+        }
+        url = f"{self.start_urls[0]}/busca?{urlencode(params, encoding='utf-8')}"
+        yield scrapy.Request(
+            url,
+            callback=self.get_last_page,
+        )
+
+    def get_last_page(self, response):
+        last_page = 1
+        has_last_page = response.css(
+            "div.publicacao ul > li.last > a::attr(data-page)"
+        ).get()
+        if has_last_page:
+            last_page = int(has_last_page)
+
+        for page in range(1, last_page + 1):
+            params = {
+                "BuscaSearch[data_inicio]": self.start_date.strftime("%Y-%m-%d"),
+                "BuscaSearch[data_fim]": self.end_date.strftime("%Y-%m-%d"),
+                "BuscaSearch[sort]": "data_new",
+                "BuscaSearch[modulo]": "diario-oficial",
+                "BuscaSearch[page]": page,
+            }
+            url = f"{self.start_urls[0]}/busca?{urlencode(params, encoding='utf-8')}"
+            yield scrapy.Request(
+                url,
+            )
 
     def parse(self, response):
-        if self.stop_crawling:
-            return
+        publications = response.css("div.publicacao > div.box-publicacao")
+        for publication in publications:
+            extra_edition = False
+            title_el = publication.css("h4 a::text").get()
+            data_el = publication.css("div div div::text").getall()[1]
+            edition_number = title_el.strip().split(" ")[-1]
+            if edition_number == "EXTRA":
+                edition_number = title_el.strip().split(" ")[-2]
+                extra_edition = True
 
-        titles = response.css(".arq-list-item-content h1::text").getall()
-        dates = response.css(".data::text").getall()
+            item_date = None
 
-        for title, data_str in zip(titles, dates):
-            edition_number = self.extrair_numero(title)
-            data_str = data_str.strip()
+            locale.setlocale(locale.LC_TIME, "pt_BR.UTF-8")
+            date_str = data_el.split(",")[-1].strip()
+            item_date = datetime.strptime(date_str, "%d de %B de %Y").date()
 
-            try:
-                item_date = datetime.strptime(data_str, "%d/%m/%Y").date()
-            except ValueError:
-                continue
-
-            if item_date < self.start_date:
-                self.stop_crawling = True
-                return
-
-            if title.endswith("."):
-                title = title[:-1]
-
-            if not title.endswith(".pdf"):
-                title += ".pdf"
-
-            file_url = f"https://maragogi.al.gov.br/wp-content/uploads/{item_date.year}/{item_date.month:02d}/{title}"
+            file_id = publication.css("h4 > a::attr(href)").get().strip().split("/")[-1]
+            url = f"{self.start_urls[0]}/diario-oficial/versao-pdf/{file_id}"
 
             yield Gazette(
                 date=item_date,
                 edition_number=edition_number,
-                is_extra_edition=False,
-                file_urls=[file_url],
+                is_extra_edition=extra_edition,
+                file_urls=[url],
                 power="executive",
             )
-
-        next_page = response.css("a.next.page-numbers::attr(href)").get()
-        if next_page:
-            yield scrapy.Request(url=next_page, callback=self.parse)
